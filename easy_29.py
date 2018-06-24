9d80679f74b078d22f33de8e871e324aff58a924
Brett McGregor <brett.w.mcgregor@gmail.com>
2018-06-25 06:06:05 +1000

    initial commit

---
+++ easy_29.py
@@ -0,0 +1,36 @@
+# A Palindrome is a sequence that is the same in
+# reverse as it is forward.
+#
+# I.e. hannah, 12321.
+#
+# Your task is to write a function to determine
+# whether a given string is palindromic or not.
+#
+# Bonus: Support multiple lines in your function
+# to validate Demetri Martin's 224 word palindrome poem.
+import os
+
+
+def palindrome(s):
+    if os.path.exists(f"{s}"):
+        with open(f"{s}", encoding="utf-8") as file:  # check if argument is a file
+            next(file)  # skip title
+            contents = file.readlines()
+            contents = "".join("".join(x.strip("\n").lower().split()) for x in contents)  # join into one string
+            p = ""
+            for char in contents:  # remove punctuation
+                if char.isalpha():
+                    p += char
+                else:
+                    continue
+            if p == p[::-1]:  # check if palindrome
+                    return True
+            return False
+    else:
+        if s == s[::-1]:  # check if string argument palindrome
+            return True
+        return False
+
+
+print(palindrome("poem.txt"))
+print(palindrome("hannah"))
